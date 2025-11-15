230c6ddf4f6914ae2c2fc5bb93123c2f7f856b88
Emanuel Jose <emanueljose0522@hotmail.com>
2025-11-15 13:23:51 -0500

    working on the file_read_commands files need to finish the echo command in the c file

---
+++ src/commands/file_read_commands.c
@@ -0,0 +1,4 @@
+#include "file_read_commands.h"
+
+void cmd_cat(int argc, char **argv);
+void cmd_echo(int argc, char **argv);
---
+++ src/commands/file_read_commands.h
@@ -0,0 +1,7 @@
+#ifndef FILE_READ_COMMANDS_H
+#define FILE_READ_COMMANDS_H
+
+void cmd_cat(int argc, char **argv);
+void cmd_echo(int argc, char **argv);
+
+#endif
