0b32a4d11767de520926ce886fd2576312436132
Emanuel Jose <emanueljose0522@hotmail.com>
2025-11-19 16:22:29 -0500

    finished the parser files

---
+++ src/parser/parser.c
@@ -0,0 +1,30 @@
+#include "parser.h"
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+//parse input into command and argument
+ParsedCommand parse_command(char *input)
+{
+    ParsedCommand cmd;
+    cmd.argc = 0;
+
+    char *token = strtok(input, " \t\n\r");
+    while (token != NULL && cmd.argc < MAX_ARGS)
+    {
+        cmd.args[cmd.argc] = strdup(token);
+        cmd.argc++;
+        token = strtok(NULL, " \t\n\r");
+    }
+    cmd.args[cmd.argc] = NULL;
+
+    return cmd;
+}
+
+void free_parsed_command(ParsedCommand *cmd)
+{
+    for(int i = 0; i < cmd->argc; i++)
+    {
+        free(cmd->args[i]);
+    }
+}
--- src/shell.c
+++ src/shell.c
@@ -1,2 +1,28 @@
 #include "shell.h"
 #include "parser/parser.h"
+#include "commands/fs_commands.h"
+#include "commands/file_read_commands.h"
+#include "commands/sys_commands.h"
+#include "commands/proc_commands.h"
+#include "commands/net_commands.h"
+#include <string.h>
+
+
+//initialize shell and run
+void shell_init(ShellState *state);
+void shell_run(ShellState *state);
+void shell_cleanup(ShellState *state);
+
+//history mamagement
+void add_to_history(ShellState *state, const char *cmd);
+void print_history(ShellState *state);
+
+//alias management
+void add_alias(ShellState *state, const char *alias, const char *command);
+const char* resolve_alias(ShellState *state, const char *cmd);
+void print_aliases(ShellState *state);
+
+//environment variable management
+void add_env_var(ShellState *state, const char *key, const char *value);
+const char* get_env_var(ShellState *state, const char *key);
+void print_env_vars(ShellState *state);
